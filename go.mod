b6fd8b7e91a23a1bf32df4d2c981eaa285ae1d01
Nat Welch <nat@natwelch.com>
2018-11-08 22:51:15 +0000

    working

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/icco/cron
+
+require (
+	github.com/icco/pinboard v0.0.0-20160202054757-76a7f4084d2b
+	github.com/machinebox/graphql v0.2.2
+	github.com/pkg/errors v0.8.0 // indirect
+	github.com/zachlatta/pin v0.0.0-20161031192518-51cb10fdcd53 // indirect
+)
--- pinboard/main.go
+++ pinboard/main.go
@@ -1,29 +1,62 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"log"
 	"os"
 	"strings"
 
+	"github.com/machinebox/graphql"
 	"github.com/zachlatta/pin"
-	//"github.com/machinebox/graphql"
 )
 
 func main() {
-
 	tokenParts := strings.Split(os.Getenv("PINBOARD_TOKEN"), ":")
 	pinClient := pin.NewClient(nil, &pin.AuthToken{Username: tokenParts[0], Token: tokenParts[1]})
 
 	tags := []string{}
-	start := 0    // 0 means beginning
-	results := 10 // 0 means all
+	start := 0   // 0 means beginning
+	results := 1 // 0 means all
 
 	posts, _, err := pinClient.Posts.All(tags, start, results, nil, nil)
 	if err != nil {
 		log.Panicf("error talking to pinboard: %+v", err)
 	}
 
+	gqlClient := graphql.NewClient("https://graphql.natwelch.com/graphql")
+	mut := `
+    mutation ($title: String!, $tags: [String!]!, $uri: URI!, $desc: String!, $time: Time!) {
+      upsertLink(
+        input: {
+          title: $title
+          tags: $tags
+          uri: $uri
+          description: $desc
+          created: $time
+        }
+      ) {
+        id
+      }
+    }
+  `
+	gqlClient.Log = func(s string) { log.Println(s) }
+
 	for _, p := range posts {
-		log.Printf("%+v", p)
+		log.Printf("%+v", p.URL)
+		req := graphql.NewRequest(mut)
+		req.Var("title", p.Title)
+		req.Var("tags", p.Tags)
+		req.Var("uri", p.URL)
+		req.Var("desc", p.Description)
+		req.Var("time", p.Time)
+		req.Header.Add("Authorization", os.Getenv("GQL_TOKEN"))
+		log.Printf("%+v", req)
+
+		var resp json.RawMessage
+		err := gqlClient.Run(context.Background(), req, &resp)
+		if err != nil {
+			log.Panicf("error talking to graphql: %+v", err)
+		}
 	}
 }
