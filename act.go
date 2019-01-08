3f389a3f167f8b47ffb02b06a066612a8f55d8be
Nat Welch <nat@natwelch.com>
2019-01-08 13:23:48 +0000

    Update act

--- act.go
+++ act.go
@@ -6,15 +6,22 @@ import (
 	"os"
 
 	"github.com/icco/cron/pinboard"
+	"github.com/icco/cron/tweets"
 )
 
 func Act(ctx context.Context, job string) error {
+	gqlToken := os.Getenv("GQL_TOKEN")
+
 	switch job {
 	case "hourly":
 	case "minute":
 	case "five-minute":
+		err := tweets.SaveUserTweets(ctx, log, gqlToken, os.Getenv("TWITTER_CONSUMER_KEY"), os.Getenv("TWITTER_CONSUMER_SECRET"), os.Getenv("TWITTER_ACCESS_TOKEN"), os.Getenv("TWITTER_ACCESS_SECRET"))
+		if err != nil {
+			return err
+		}
 	case "fifteen-minute":
-		err := pinboard.UpdatePins(ctx, log, os.Getenv("PINBOARD_TOKEN"), os.Getenv("GQL_TOKEN"))
+		err := pinboard.UpdatePins(ctx, log, os.Getenv("PINBOARD_TOKEN"), gqlToken)
 		if err != nil {
 			return err
 		}
