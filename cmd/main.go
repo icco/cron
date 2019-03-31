8728be52d6062dc8f143c0603f7e7456ce54a0ad
Nat Welch <nat@natwelch.com>
2019-03-31 03:22:40 +0000

    progress I guess

--- cmd/main.go
+++ cmd/main.go
@@ -3,46 +3,26 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
 
-	"github.com/c-bata/go-prompt"
 	"github.com/icco/cron"
 )
 
-func completer(d prompt.Document) []prompt.Suggest {
-	s := []prompt.Suggest{
-		{Text: "run", Description: "Run a certain command set."},
-	}
-	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
-}
-
-func executor(in string) {
-	in = strings.TrimSpace(in)
-
-	blocks := strings.Split(in, " ")
-	switch blocks[0] {
-	case "run":
-		if len(blocks) != 2 {
-			fmt.Println("Sorry, I don't understand.")
-			return
-		}
-		err := cron.Act(context.Background(), blocks[1])
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		return
-	default:
-		fmt.Println("Sorry, I don't understand.")
-		return
-	}
-}
+var (
+	log = cron.InitLogging()
+)
 
 func main() {
-	p := prompt.New(
-		executor,
-		completer,
-		prompt.OptionPrefix("> "),
-		prompt.OptionTitle("cron test"),
-	)
-	p.Run()
+	cmd := os.Args[1:]
+	if len(cmd) < 2 && cmd[0] != "send" {
+		fmt.Printf("Usage: $ %s send message", os.Args[0])
+		return
+	}
+
+	err := cron.Act(context.Background(), strings.Join(cmd[1:], " "))
+	if err != nil {
+		log.WithError(err).Error(err.Error())
+		return
+	}
 }
