89b3ad58bed8175fd2eb70ffbb3e18fbec428434
Nat Welch <nat@natwelch.com>
2019-05-21 09:29:37 +0000

    also doesn't work

--- spider/spider.go
+++ spider/spider.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"golang.org/x/net/html"
@@ -21,39 +22,37 @@ var (
 func Crawl(conf *Config) {
 	c = conf
 
-	// Create channels for message passing.
 	messages := make(chan string, 100)
 
 	// Pass in init url
 	messages <- c.URL
-	ctx := context.TODO()
+	ctx, cncl := context.WithTimeout(context.Background(), 30*time.Second)
 
-	go worker(ctx, messages)
+	worker(ctx, messages)
+	cncl()
 }
 
-func worker(ctx context.Context, msgChan <-chan string) {
-	c.Log.Debug("work")
-	for {
-		select {
-		case msg := <-msgChan:
-			urls, err := ScrapeUrl(msg)
-			if err != nil {
-				c.Log.WithError(err).WithContext(ctx).Error("scrape error")
-			}
+func worker(ctx context.Context, msgChan chan string) {
+	for msg := range msgChan {
+		urls, err := ScrapeUrl(msg)
+		if err != nil {
+			c.Log.WithError(err).WithContext(ctx).Error("scrape error")
+		}
 
-			for _, u := range urls {
-				//msgChan <- u
-				c.Log.Debug(u)
-			}
-		case <-ctx.Done():
+		for _, u := range urls {
+			msgChan <- u
+		}
+
+		if ctx.Err() != nil {
 			c.Log.Warn(ctx.Err())
 			return
 		}
 	}
+
+	return
 }
 
 func ScrapeUrl(uri string) ([]string, error) {
-	c.Log.Infof("visiting %+v", uri)
 	response, err := http.Get(uri)
 	ret := []string{}
 
@@ -82,7 +81,6 @@ func ScrapeUrl(uri string) ([]string, error) {
 							continue
 						} else {
 							if u.IsAbs() {
-								c.Log.Debugf("found %+v", attr.Val)
 								ret = append(ret, attr.Val)
 							}
 						}
