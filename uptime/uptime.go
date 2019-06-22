381e9d887df8fc72b2edb3b84eac921e275f42c1
Nat Welch <nat@natwelch.com>
2019-06-22 23:15:46 +0000

    update func

--- uptime/uptime.go
+++ uptime/uptime.go
@@ -34,33 +34,35 @@ func UpdateUptimeChecks(ctx context.Context, c *Config) error {
 	for _, s := range updater.AllSites {
 		hosts = append(hosts, s.Host)
 	}
-
 	sort.Strings(hosts)
 
 	existingChecks, err := c.list(ctx)
 	if err != nil {
 		return err
 	}
+	existingHosts := []string{}
+	checkHostMap := map[string]string{}
 
 	for _, check := range existingChecks {
 		mr := check.GetMonitoredResource()
-		c.Log.Debugf("host found: %+v", mr.Labels["host"])
-		i := sort.SearchStrings(hosts, mr.Labels["host"])
-		if i >= 0 && i < len(hosts) {
-			hosts = remove(hosts, i)
+		host := mr.Labels["host"]
+		c.Log.Debugf("host found: %+v", host)
+		existingHosts = append(existingHosts, host)
+		checkHostMap[host] = check.Name
+	}
+	sort.Strings(existingHosts)
+
+	for _, host := range hosts {
+		i := sort.SearchStrings(existingHosts, host)
+
+		if i >= len(existingHosts) {
+			c.create(ctx, host)
+		} else {
+			c.update(ctx, host, checkHostMap[host])
 		}
 	}
 
-	for _, host := range hosts {
-		c.create(ctx, host)
-	}
-
 	return nil
-}
-
-func remove(s []string, i int) []string {
-	s[i] = s[len(s)-1]
-	return s[:len(s)-1]
 }
 
 // create creates an example uptime check.
@@ -95,11 +97,7 @@ func (c *Config) create(ctx context.Context, host string) (*monitoringpb.UptimeC
 		},
 	}
 	c.Log.Infof("creating %+v", req)
-	config, err := client.CreateUptimeCheckConfig(ctx, req)
-	if err != nil {
-		return nil, err
-	}
-	return config, nil
+	return client.CreateUptimeCheckConfig(ctx, req)
 }
 
 // list is an example of listing the uptime checks in projectID.
@@ -128,34 +126,40 @@ func (c *Config) list(ctx context.Context) ([]*monitoringpb.UptimeCheckConfig, e
 	return ret, nil
 }
 
-// update is an example of updating an uptime check. resourceName should be
-// of the form `projects/[PROJECT_ID]/uptimeCheckConfigs/[UPTIME_CHECK_ID]`.
-//func update(w io.Writer, resourceName, displayName, httpCheckPath string) (*monitoringpb.UptimeCheckConfig, error) {
-//	ctx := context.Background()
-//	client, err := monitoring.NewUptimeCheckClient(ctx)
-//	if err != nil {
-//		return nil, fmt.Errorf("NewUptimeCheckClient: %v", err)
-//	}
-//	defer client.Close()
-//	getReq := &monitoringpb.GetUptimeCheckConfigRequest{
-//		Name: resourceName,
-//	}
-//	config, err := client.GetUptimeCheckConfig(ctx, getReq)
-//	if err != nil {
-//		return nil, fmt.Errorf("GetUptimeCheckConfig: %v", err)
-//	}
-//	config.DisplayName = displayName
-//	config.GetHttpCheck().Path = httpCheckPath
-//	req := &monitoringpb.UpdateUptimeCheckConfigRequest{
-//		UpdateMask: &field_mask.FieldMask{
-//			Paths: []string{"display_name", "http_check.path"},
-//		},
-//		UptimeCheckConfig: config,
-//	}
-//	config, err = client.UpdateUptimeCheckConfig(ctx, req)
-//	if err != nil {
-//		return nil, fmt.Errorf("UpdateUptimeCheckConfig: %v", err)
-//	}
-//	fmt.Fprintf(w, "Successfully updated %v", resourceName)
-//	return config, nil
-//}
+// update is an example of updating an uptime check.
+func (c *Config) update(ctx context.Context, host, id string) (*monitoringpb.UptimeCheckConfig, error) {
+	client, err := monitoring.NewUptimeCheckClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+	getReq := &monitoringpb.GetUptimeCheckConfigRequest{
+		Name: id,
+	}
+	config, err := client.GetUptimeCheckConfig(ctx, getReq)
+	if err != nil {
+		return nil, err
+	}
+	config.DisplayName = host
+	config.Resource = &monitoringpb.UptimeCheckConfig_MonitoredResource{
+		MonitoredResource: &monitoredres.MonitoredResource{
+			Type: "uptime_url",
+			Labels: map[string]string{
+				"host": host,
+			},
+		},
+	}
+	config.CheckRequestType = &monitoringpb.UptimeCheckConfig_HttpCheck_{
+		HttpCheck: &monitoringpb.UptimeCheckConfig_HttpCheck{
+			Path:   "/",
+			Port:   443,
+			UseSsl: true,
+		},
+	}
+	config.Timeout = &duration.Duration{Seconds: 5}
+	config.Period = &duration.Duration{Seconds: 60}
+	req := &monitoringpb.UpdateUptimeCheckConfigRequest{
+		UptimeCheckConfig: config,
+	}
+	return client.UpdateUptimeCheckConfig(ctx, req)
+}
