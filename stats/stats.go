2a7c617b432f76ab201a7e885b9d98a90f7f2388
Nat Welch <nat@natwelch.com>
2020-12-26 02:36:20 +0000

    Revert "stop timeouts"
    This reverts commit ad32ac31b6c818c7da5171ebc40a0fd072ed392f.

--- stats/stats.go
+++ stats/stats.go
@@ -48,21 +48,20 @@ func (c *Config) Update(ctx context.Context) error {
 		})
 	}
 
-	// TODO: Stop this from timing out
-	//g.Go(func() error {
-	//	stats, err := GetCounts(ctx, c)
-	//	if err != nil {
-	//		return fmt.Errorf("get counts: %w", err)
-	//	}
+	g.Go(func() error {
+		stats, err := GetCounts(ctx, c)
+		if err != nil {
+			return fmt.Errorf("get counts: %w", err)
+		}
 
-	//	for _, s := range stats {
-	//		if err := c.UploadStat(ctx, s.Key, s.Value); err != nil {
-	//			return fmt.Errorf("upload stat: %w", err)
-	//		}
-	//	}
+		for _, s := range stats {
+			if err := c.UploadStat(ctx, s.Key, s.Value); err != nil {
+				return fmt.Errorf("upload stat: %w", err)
+			}
+		}
 
-	//	return nil
-	//})
+		return nil
+	})
 
 	return g.Wait()
 }
