77f842e8da69915e096df1da8ea904948223094c
Nat Welch <nat@natwelch.com>
2021-04-15 11:55:07 +0000

    update random site

--- act.go
+++ act.go
@@ -86,6 +86,10 @@ func (cfg *Config) Act(octx context.Context, job string) error {
 		if err := cfg.UpdateTriggers(ctx); err != nil {
 			return fmt.Errorf("update triggers: %w", err)
 		}
+
+		if err := cfg.UpdateRandomSite(ctx); err != nil {
+			return fmt.Errorf("update random site: %w", err)
+		}
 	case "spider":
 		spider.Crawl(ctx, &spider.Config{
 			Log: cfg.Log,
---
+++ updater/git.go
@@ -0,0 +1,50 @@
+package updater
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	cloudbuild "cloud.google.com/go/cloudbuild/apiv1/v2"
+	"github.com/icco/cron/sites"
+	cloudbuildpb "google.golang.org/genproto/googleapis/devtools/cloudbuild/v1"
+)
+
+// Update runs a trigger to update a site.
+func (cfg *Config) Update(ctx context.Context, site sites.SiteMap) error {
+	c, err := cloudbuild.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("could not create client: %w", err)
+	}
+
+	name := fmt.Sprintf(deployerFormat, site.Deployment)
+	trig, err := c.GetBuildTrigger(ctx, &cloudbuildpb.GetBuildTriggerRequest{
+		ProjectId: cfg.GoogleProject,
+		TriggerId: name,
+	})
+	if err != nil {
+		return fmt.Errorf("get build trigger %q: %w", name, err)
+	}
+
+	op, err := c.RunBuildTrigger(ctx, &cloudbuildpb.RunBuildTriggerRequest{
+		ProjectId: cfg.GoogleProject,
+		TriggerId: trig.Id,
+	})
+	if err != nil {
+		return fmt.Errorf("run build trigger: %w", err)
+	}
+
+	if _, err := op.Wait(ctx); err != nil {
+		return fmt.Errorf("build failed: %w", err)
+	}
+
+	return nil
+}
+
+// UpdateRandomSite updates one site picked randomly.
+func (cfg *Config) UpdateRandomSite(ctx context.Context) error {
+	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
+	s := sites.All[rand.Intn(len(sites.All))]
+	return cfg.Update(ctx, s)
+}
