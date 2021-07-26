539fe0ead7df83f3db1db9238cb54c95b04f3f7d
Nat Welch <nat@natwelch.com>
2021-07-26 00:14:16 +0000

    github

--- gaudit/gh.go
+++ gaudit/gh.go
@@ -5,18 +5,26 @@ import (
 	"fmt"
 
 	"github.com/google/go-github/v37/github"
+	"github.com/icco/cron/shared"
 )
 
-func checkRepos(ctx context.Context) error {
+type Config struct {
+	shared.Config
+}
+
+func (c *Config) checkRepos(ctx context.Context) error {
 	client := github.NewClient(nil)
 	opt := &github.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
 	user := "icco"
 
 	repos, _, err := client.Repositories.List(ctx, user, opt)
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 
-	fmt.Printf("Recently updated repositories by %q: %v", user, github.Stringify(repos))
+	for _, r := range repos {
+		c.Log.Infow(fmt.Sprintf("%s/%s", user, r.GetName()), "repo", r)
+	}
 
+	return nil
 }
